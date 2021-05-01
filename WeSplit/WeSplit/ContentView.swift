b547c8bdb6b338488acb559d1350f95ede2a4bc1
Ivan Ivanusic <ivanivanusic98@gmail.com>
2021-05-01 15:23:29 +0200

    Project 1 - Day 1

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ivan Ivanušić on 01.05.2021..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
