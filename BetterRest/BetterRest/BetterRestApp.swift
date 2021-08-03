488d3526a6b8aba25cedf4fbb1f7680e6bdfd110
Ivan Ivanusic <ivanivanusic98@gmail.com>
2021-08-03 22:45:26 +0200

    Project 4 - Day 1

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ivan Ivanušić on 03.08.2021..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
