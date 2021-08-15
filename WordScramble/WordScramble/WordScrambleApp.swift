defb14773d931b6d107de25b45b345f18e31544a
Ivan Ivanusic <ivanivanusic98@gmail.com>
2021-08-15 15:34:32 +0200

    Project 5 - Day 1

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ivan Ivanušić on 15.08.2021..
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
