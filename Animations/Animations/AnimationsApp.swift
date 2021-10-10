7ec637639e23a5b334fd564ca4e32b63eeeb1464
Ivan Ivanusic <ivanivanusic98@gmail.com>
2021-10-10 22:45:09 +0200

    Project 6 - Day 1

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ivan Ivanušić on 10.10.2021..
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
