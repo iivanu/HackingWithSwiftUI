d53e17a68af0ca08fc8bc3acd18d90549032a229
Ivan Ivanusic <ivanivanusic98@gmail.com>
2021-10-11 22:07:49 +0200

    Project 6 - Done

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,9 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var animationAmount = 0.0
+    @State private var opacity = 1.0
+    
     var body: some View {
-        Text("Hello, world! ")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                    .rotation3DEffect(.degrees(number == correctAnswer ? animationAmount : 0), axis: (x: 0, y: 1, z: 0))
+                    .opacity(number == correctAnswer ? 1 : opacity)
+                    .animation(number == correctAnswer ? nil : .easeOut, value: 1)
+                }
+                VStack(spacing: 30) {
+                    Text("Score: \(score)")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your score is \(score)"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+            
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+            withAnimation {
+                animationAmount += 360
+                opacity = 0.25
+            }
+        } else {
+            scoreTitle = "Wrong! That’s the flag of \(countries[number])."
+            score -= 1
+            withAnimation {
+                opacity = 0.25
+            }
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        opacity = 1
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
@@ -19,3 +94,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
