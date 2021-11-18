a1a7f5678f91ad85df8f9a69058e7bd2260ccc26
Ivan Ivanusic <ivanivanusic98@gmail.com>
2021-11-18 19:53:59 +0100

    Project 7 - Day 1

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Ivan Ivanušić on 18.11.2021..
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
